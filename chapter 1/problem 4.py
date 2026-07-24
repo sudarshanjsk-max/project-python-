7e99f173d94b459e328d710f216bde6e6cb815f6
sudarshanjsk-stack <sudarshanjsk@gmail.com>
2026-07-24 23:36:32 +0530

    file modified

--- chapter 1/problem 4.py
+++ chapter 1/problem 4.py
@@ -4,18 +4,23 @@ import pyttsx3
 engine = pyttsx3.init()
 
 # Queue the text you want the computer to say
-engine.say("hi my name is Sudarshan and my favourite anime is solo leveling")
+engine.say("sudarshan is a good boy")
 
 # Process and run the speech commands
 engine.runAndWait()
+    
+
 
 import os
 
 # specify the path (use "." for current directory)
-path = "/New folder"
+path = "./New folder"
 # list all files and folders in the directory
 contents = os.listdir(path)
 
 # print them one by one
 for item in contents:
     print(item)
+    print("is a file" if os.path.isfile(os.path.join(path, item)) else  " is a folder")
+    # file modified to pyttsx3 to say the name of the file or folder
+    engine .say(item)
--- chapter 2/problem 5.py
+++ chapter 2/problem 5.py
@@ -1,4 +1,4 @@
 a=int(input("enter the first number"))
 b=int(input("enter the second number"))
 c=(a+b)/2
-print("the average of the two numbers is",c)
+print("the average of the two numbers mentioned is",c)
